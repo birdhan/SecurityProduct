3caf9a648f07573a056c8f4a9e4b52cd84b55803
huan666 <1370678768@qq.com>
2018-12-30 22:56:59 +0800

    pojo11

---
+++ Linsnacks/src/lin/snacks/pojo/User.java
@@ -0,0 +1,44 @@
+package lin.snacks.pojo;
+	
+	public class User {
+		private String id;
+		private String name;
+		private String tel;
+		private String sex;
+		private String address;
+		public String getId() {
+			return id;
+		}
+		public void setId(String id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getTel() {
+			return tel;
+		}
+		public void setTel(String tel) {
+			this.tel = tel;
+		}
+		public String getSex() {
+			return sex;
+		}
+		public void setSex(String sex) {
+			this.sex = sex;
+		}
+		public String getAddress() {
+			return address;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		@Override
+		public String toString() {
+			return "User [id=" + id + ", name=" + name + ", tel=" + tel + ", sex=" + sex + ", address=" + address + "]";
+		}
+	
+	}
